31a80d1fd007d14bbff7572203fde79dba7e5aa7
Joel Tony <joeltony72@gmail.com>
2022-09-05 10:26:51 +0530

    Lab1 Pattern 1

---
+++ Lab1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Lab1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("n = ");
+		int n = sc.nextInt();
+		System.out.println("Pattern 1: ");
+		pattern1(n);
+		System.out.println();
+		System.out.println("Pattern 2: ");
+		// pattern2(n);
+		sc.close();
+	}
+
+	public static void pattern1(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < (n - i); j++)
+				System.out.print(" ");
+			for (int j = 0; j < i; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+
+	public static void pattern2(int n) {
+		// TODO
+	}
+}
