60be8a8c30d44036c4631f643cca60c03a6d4077
Joel Tony <joeltony72@gmail.com>
2022-09-23 10:13:44 +0530

    Add Lecture3_Practice

---
+++ Lecture3_Practice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Lecture3_Practice {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String[] splitStr = str.trim().split("\\s+");
+		for (int i = 0; i < splitStr.length; i++)
+			System.out.print(splitStr[i] + ' ');
+		sc.close();
+	}
+}
