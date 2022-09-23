e97f480db07ae861a2d7e041e852cc776b626ac6
Joel Tony <joeltony72@gmail.com>
2022-09-23 10:21:17 +0530

    Lecture3_Practice: Replace whitespace regex with " "

--- Lecture3_Practice.java
+++ Lecture3_Practice.java
@@ -4,9 +4,10 @@ public class Lecture3_Practice {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-		String[] splitStr = str.trim().split("\\s+");
+		String[] splitStr = str.trim().split(" ");
 		for (int i = 0; i < splitStr.length; i++)
 			System.out.print(splitStr[i] + ' ');
+		System.out.println();
 		sc.close();
 	}
 }
