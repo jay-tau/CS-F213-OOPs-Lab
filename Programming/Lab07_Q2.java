a6bb5352b1a20d8355cbeffb767b383d86a7183b
Joel Tony <joeltony72@gmail.com>
2022-12-30 00:24:04 +0530

    Add tests for Lab07_Q2
    The tests are based on the sample input and output provided in the question.

--- Programming/Lab07_Q2.java
+++ Programming/Lab07_Q2.java
@@ -1,3 +1,66 @@
+/*
+ * We have three classes - Shape, Circle, Rectangle.
+ *
+ * You have to complete the abstract class, Shape, by adding the following abstract methods:
+ *
+ * public double getArea()
+ *
+ * public boolean fitOtherShape(Shape shape)
+ *
+ * For the Circle class, you have to extend the Shape class and add the following:
+ *
+ * Create the constructor with the given parameters.
+ * Implement the getArea method for the circle and return a double value (rounded upto two decimal places).
+ * Implement the fitOtherShape method, where the method checks whether the current circle can fit another circle or a rectangle inside it. (Perfect Overlap is allowed)
+ * Override the toString method by returning the following “Circle <name> having area <value>”
+ * Do the same work for Rectangle class.
+ *
+ * For the public class Question2, implement the following method:
+ *
+ * public static void smallerShapes(int index, Shapes[] shapes) : Array shapes have all the input shapes. Method should print the count of the shapes that can fit inside the shape at the given index. It should also print each shape in order, as they appear in the shapes array.
+ *
+ * Note:
+ *
+ * DO NOT modify any of the base code. Only add your code in the space mentioned.
+ * <Tag> denotes that they have to be replaced with the respective input.
+ * Input format:
+ *
+ * The first line is the number of shapes to be input.
+ * For every shape, there are 2 lines:
+ *
+ * First line is the shape type, whether Circle or Rectangle
+ *
+ * Second line is the name and radius, for Circle and name, length and width for Rectangle.
+ *
+ * Last line is the index of the shape that is to be compared with other shapes, other than itself. (indexed from 1)
+ * Output format:
+ *
+ * The first line displays the number of shapes that can fit inside shapes[index]
+ *
+ * The next lines print the shapes that can fit inside.
+ * Sample Input:
+ *
+ * 3
+ * Circle
+ * c1 2
+ * Rectangle
+ * r1 4 4
+ * Circle
+ * c2 3
+ * 2
+ * Sample Output:
+ *
+ * 1
+ * Circle c1 having area 12.57
+ * Explanation:
+ *
+ * Rectangle r1 has to be compared with other shapes, as index is 2.
+ *
+ * Circle c1 can fit inside Rectangle since the diameter of the circle is 4 and length and width of the rectangle is also 4.
+ *
+ * Circle c2 has a larger diameter, i.e, 6 so it can’t fit inside the rectangle.
+ */
+
 import java.util.Scanner;
 
 abstract class Shape {
