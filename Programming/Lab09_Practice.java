91f1f68e33049a16a0a1dfee1c197a9be9a5b927
Joel Tony <joeltony72@gmail.com>
2022-12-30 14:13:17 +0530

    Fixes #1
    The problem statement says that the longest valid substring is the one with the maximum length.
    
    The previous solution was returning the length of the longest valid substring starting from the beginning of the string.
    
    This commit fixes that.

--- Programming/Lab09_Practice.java
+++ Programming/Lab09_Practice.java
@@ -24,14 +24,31 @@
 import java.util.*;
 
 public class Lab09_Practice {
+    public static int solve(String s) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int max_len = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char current_char = s.charAt(i);
+            if (current_char == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    max_len = Math.max(max_len, i - st.peek());
+                }
+            }
+        }
+        return max_len;
+    }
+
     public static void main(String[] args) {
         String s;
         Scanner sc = new Scanner(System.in);
         s = sc.next();
-        /*
-         *
-         * Write your code here
-         *
-         */
+        System.out.println(solve(s));
+        sc.close();
     }
 }
